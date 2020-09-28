3fb68d10dbd04ba81f2faea1225ace610f285519
jago <johnalexguerrero@gmail.com>
2020-09-28 17:27:17 -0500

    configuracion django

---
+++ mystore/provider/models.py
@@ -0,0 +1,17 @@
+from django.db import models
+from ckeditor.fields import RichTextField
+
+
+class Empresa(models.Model):
+    name = models.CharField(max_length=200)
+    date_created = models.DateTimeField('date created')
+    status = models.BooleanField('Status',default=True)
+    def __str__(self):
+        return self.name
+
+
+'''class Factura(models.Model):
+    empresa = models.ForeignKey(Empresa, on_delete=models.CASCADE)
+     = models.CharField(max_length=200)
+    votes = models.IntegerField(default=0)'''
+# Create your models here.
